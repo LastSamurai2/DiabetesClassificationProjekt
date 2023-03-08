051ddf1816ab43a157b02d4020bb864a4bf13c45
LastSamurai2 <44116639+LastSamurai2@users.noreply.github.com>
2023-03-08 13:22:09 +0100

    Initial commit

---
+++ diabetes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class DiabetesData(models.Model):
+    pregnancies = models.IntegerField(null=False, blank=False),
+    glucose = models.IntegerField(null=False, blank=False),
+    blood_preasure = models.IntegerField(null=False, blank=False),
+    skin_thickness = models.IntegerField(null=False, blank=False),
+    insulin = models.IntegerField(null=False, blank=False),
+    bmi = models.FloatField(null=False, blank=False),
+    diabetes_pedigree_function = models.FloatField(null=False, blank=False),
+    age = models.IntegerField(null=False, blank=False),
+    outcome = models.IntegerField(null=False, blank=False),
